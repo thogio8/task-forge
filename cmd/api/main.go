1c2445c6978faaca6c7ee228a42b41c3ff46ec10
Thomas Gioana <thomas.gioana@api-and-you.com>
2026-03-02 14:16:40 +0100

    Use the the config file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,29 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
+	"github.com/thogio8/task-forge/internal/config"
 	"github.com/thogio8/task-forge/internal/handler"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using environment variables")
+	cfg, err := config.Load()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	dsn := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
-	)
-
-	db, err := pgxpool.New(context.Background(), dsn)
+	db, err := pgxpool.New(context.Background(), cfg.DatabaseURL())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,8 +41,8 @@ func main() {
 	router.Post("/tasks", taskHandler.CreateTask)
 	router.Get("/tasks", taskHandler.GetTasks)
 
-	log.Println("Server running on :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Printf("Server running on port : %s", cfg.HTTPPort)
+	if err := http.ListenAndServe(":"+cfg.HTTPPort, router); err != nil {
 		log.Fatal(err)
 	}
 }
