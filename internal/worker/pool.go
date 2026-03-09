6a60e3a9f6803f455774c586a757d455c1c7d201
Thomas Gioana <thomas.gioana@api-and-you.com>
2026-03-09 11:36:13 +0100

    feat(worker): add basic worker pool with channel dispatch

---
+++ internal/worker/pool.go
@@ -0,0 +1,44 @@
+package worker
+
+import (
+	"log/slog"
+	"sync"
+
+	"github.com/thogio8/task-forge/internal/model"
+)
+
+type Pool struct {
+	workerCount int
+	processFunc func(model.Task)
+	tasks       chan model.Task
+	wg          sync.WaitGroup
+	logger      *slog.Logger
+}
+
+func NewPool(workerCount int, processFunc func(model.Task), logger *slog.Logger) *Pool {
+	bufferedChannel := make(chan model.Task, workerCount*2)
+
+	return &Pool{workerCount: workerCount, processFunc: processFunc, tasks: bufferedChannel, logger: logger}
+}
+
+func (p *Pool) Start() {
+	p.wg.Add(p.workerCount)
+
+	for i := range p.workerCount {
+		go func(index int) {
+			defer p.wg.Done()
+			for task := range p.tasks {
+				p.processFunc(task)
+			}
+		}(i)
+	}
+}
+
+func (p *Pool) Submit(task model.Task) {
+	p.tasks <- task
+}
+
+func (p *Pool) Stop() {
+	close(p.tasks)
+	p.wg.Wait()
+}
---
+++ internal/worker/pool_test.go
@@ -0,0 +1,58 @@
+package worker
+
+import (
+	"log/slog"
+	"os"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/thogio8/task-forge/internal/model"
+)
+
+var testLogger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+func TestPool_AllTasksProcessed(t *testing.T) {
+	var counter atomic.Int64
+
+	workerCount := 3
+	processFunc := func(_ model.Task) { counter.Add(1) }
+
+	pool := NewPool(workerCount, processFunc, testLogger)
+
+	pool.Start()
+
+	for range 20 {
+		pool.Submit(model.Task{})
+	}
+
+	pool.Stop()
+
+	if counter.Load() != 20 {
+		t.Fatalf("expected counter to be 20, got: %d", counter.Load())
+	}
+}
+
+func TestPool_StopBlocksUntilDone(t *testing.T) {
+	var counter atomic.Int64
+
+	workerCount := 3
+	processFunc := func(_ model.Task) {
+		time.Sleep(50 * time.Millisecond)
+		counter.Add(1)
+	}
+
+	pool := NewPool(workerCount, processFunc, testLogger)
+
+	pool.Start()
+
+	for range 10 {
+		pool.Submit(model.Task{})
+	}
+
+	pool.Stop()
+
+	if counter.Load() != 10 {
+		t.Fatalf("expected counter to be 10, got: %d", counter.Load())
+	}
+}
